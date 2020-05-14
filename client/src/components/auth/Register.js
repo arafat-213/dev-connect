bac283e442cd3110a2104ad392582635b7992fd7
arafat-213 <tai.arafat.at@gmail.com>
2020-05-14 11:40:08 +0530

    added registration form on client

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,90 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { useState } from 'react'
 
 const Register = () => {
-	return <div></div>
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		password: '',
+		password2: ''
+	})
+
+	const { name, email, password, password2 } = formData
+
+	const onChange = e =>
+		setFormData({ ...formData, [e.target.name]: e.target.value })
+
+	const onSubmit = e => {
+		e.preventDefault()
+		if (password !== password2) {
+			console.log('Passwords do not match')
+		} else {
+			console.log(formData)
+		}
+	}
+	return (
+		<Fragment>
+			<section className='container'>
+				<h1 className='large text-primary'>Sign Up</h1>
+				<p className='lead'>
+					<i className='fas fa-user'></i> Create Your Account
+				</p>
+				<form className='form' onSubmit={e => onSubmit(e)}>
+					<div className='form-group'>
+						<input
+							type='text'
+							placeholder='Name'
+							name='name'
+							value={name}
+							onChange={onChange}
+							required
+						/>
+					</div>
+					<div className='form-group'>
+						<input
+							type='email'
+							placeholder='Email Address'
+							name='email'
+							value={email}
+							onChange={onChange}
+						/>
+						<small className='form-text'>
+							This site uses Gravatar so if you want a profile
+							image, use a Gravatar email
+						</small>
+					</div>
+					<div className='form-group'>
+						<input
+							type='password'
+							placeholder='Password'
+							name='password'
+							minLength='6'
+							value={password}
+							onChange={onChange}
+						/>
+					</div>
+					<div className='form-group'>
+						<input
+							type='password'
+							placeholder='Confirm Password'
+							name='password2'
+							minLength='6'
+							value={password2}
+							onChange={onChange}
+						/>
+					</div>
+					<input
+						type='submit'
+						className='btn btn-primary'
+						value='Register'
+					/>
+				</form>
+				<p className='my-1'>
+					Already have an account? <a href='login.html'>Sign In</a>
+				</p>
+			</section>
+		</Fragment>
+	)
 }
 
 export default Register
