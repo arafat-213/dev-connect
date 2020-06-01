0f7877cb6b35de560e258fdc59804591cb55cad5
arafat-213 <tai.arafat.at@gmail.com>
2020-06-01 15:48:33 +0530

    added profile reducer and get current profile action

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,35 @@
+import { GET_PROFILE, PROFILE_ERROR } from '../actions/types'
+
+/* *
+ * @params profile : profile of user logged in or the another user whose profile a user visits
+ * @params profiles : list of all profiles to display on profiles page
+ * @params repos : github repos of current user
+ * @params loading: request loading staus
+ * @params error : error returned by backend
+ */
+const initailState = {
+	profile: null,
+	profiles: [],
+	repos: [],
+	loading: true,
+	error: {}
+}
+export default function (state = initailState, action) {
+	const { type, payload } = action
+	switch (type) {
+		case GET_PROFILE:
+			return {
+				...state,
+				profile: payload,
+				loading: false
+			}
+		case PROFILE_ERROR:
+			return {
+				...state,
+				error: payload,
+				loading: false
+			}
+		default:
+			return state
+	}
+}
