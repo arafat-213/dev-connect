42302e8da27141d03846a62aaa8be787d13b1bfd
arafat-213 <tai.arafat.at@gmail.com>
2020-06-02 12:01:36 +0530

    added create profile feature on client

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { useState, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { createProfile } from '../../actions/profile'
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
 	const [formData, setFormData] = useState({
 		company: '',
 		website: '',
@@ -37,7 +39,10 @@ const CreateProfile = props => {
 
 	const onChange = e =>
 		setFormData({ ...formData, [e.target.name]: e.target.value })
-
+	const onSubmit = e => {
+		e.preventDefault()
+		createProfile(formData, history)
+	}
 	return (
 		<Fragment>
 			<h1 className='large text-primary'>Create Your Profile</h1>
@@ -46,9 +51,12 @@ const CreateProfile = props => {
 				make your profile stand out
 			</p>
 			<small>* = required field</small>
-			<form className='form'>
+			<form className='form' onSubmit={e => onSubmit(e)}>
 				<div className='form-group'>
-					<select name='status'>
+					<select
+						name='status'
+						value={githubusername}
+						onChange={e => onChange(e)}>
 						<option value='0'>* Select Professional Status</option>
 						<option value='Developer'>Developer</option>
 						<option value='Junior Developer'>
@@ -222,6 +230,8 @@ const CreateProfile = props => {
 	)
 }
 
-CreateProfile.propTypes = {}
+CreateProfile.propTypes = {
+	createProfile: PropTypes.func.isRequired
+}
 
-export default connect()(CreateProfile)
+export default connect(null, { createProfile })(withRouter(CreateProfile))
