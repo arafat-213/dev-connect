db7f35ed8e24bda8e573092e3cb5ee2e11fad0b2
arafat-213 <tai.arafat.at@gmail.com>
2020-06-02 10:58:30 +0530

    added create profile form

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Alert from './components/layout/Alert'
 import setAuthToken from './utils/setAuthToken'
 import Dashboard from './components/dashboard/Dashboard'
 import PrivateRoute from './components/routing/PrivateRoute'
+import CreateProfile from './components/profile-forms/CreateProfile'
 // Redux
 import { Provider } from 'react-redux'
 import store from './store'
@@ -40,6 +41,11 @@ const App = () => {
 								exact
 								path='/dashboard'
 								component={Dashboard}
+							/>
+							<PrivateRoute
+								exact
+								path='/create-profile'
+								component={CreateProfile}
 							/>
 						</Switch>
 					</section>
