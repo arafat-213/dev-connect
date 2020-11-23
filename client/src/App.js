2d8fb53e9938a5021a440f9567312eda02161112
arafat-213 <tai.arafat.at@gmail.com>
2020-11-23 22:04:27 +0530

    added posts on client

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import CreateProfile from './components/profile-forms/CreateProfile'
 import EditProfile from './components/profile-forms/EditProfile'
 import AddExperience from './components/profile-forms/AddExperience'
 import AddEducation from './components/profile-forms/AddEducation'
+import Posts from './components/posts/Posts'
 
 // Redux
 import { Provider } from 'react-redux'
@@ -79,6 +80,11 @@ const App = () => {
 								exact
 								path='/add-education'
 								component={AddEducation}
+							/>
+							<PrivateRoute
+								exact
+								path='/posts'
+								component={Posts}
 							/>
 						</Switch>
 					</section>
---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,58 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import Moment from 'react-moment'
+import { connect } from 'react-redux'
+
+const PostItem = ({
+	auth,
+	post: { _id, text, name, avatar, user, likes, comments, date }
+}) => {
+	return (
+		<div className='post bg-white p-1 my-1'>
+			<div>
+				<a href='profile.html'>
+					<img src={avatar} alt='user avatar' className='round-img' />
+					<h4>{name}</h4>
+				</a>
+			</div>
+			<div>
+				<p className='my-1'>{text}</p>
+				<p className='post-date'>
+					Posted at <Moment format='HH:MM, DD-MM-YYYY'>{date}</Moment>
+				</p>
+				<button className='btn btn-light' type='button'>
+					<i className='fas fa-thumbs-up'></i>
+					{likes.length > 0 && <span> {likes.length}</span>}
+				</button>
+				<button className='btn btn-light' type='button'>
+					<i className='fas fa-thumbs-down'></i>
+				</button>
+				<Link to={`/posts/${_id}`} className='btn btn-primary'>
+					Discussion{' '}
+					{comments.length > 0 && (
+						<span className='comment-count'>{comments.length}</span>
+					)}
+				</Link>
+				{!auth.loading && user === auth.user._id && (
+					<button className='btn btn-danger' type='button'>
+						<i className='fas fa-times'></i>
+					</button>
+				)}
+			</div>
+		</div>
+	)
+}
+
+PostItem.propTypes = {
+	post: PropTypes.object.isRequired,
+	auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => {
+	return {
+		auth: state.auth
+	}
+}
+
+export default connect(mapStateToProps)(PostItem)
